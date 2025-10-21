7767469bb2e8c7dd0a3e62f1d820e10882e72aa9
MHMDHISHAM17 <muhammedhisham7994@gmail.com>
2025-10-21 09:38:03 +0530

    commit initial files

---
+++ index.js
@@ -0,0 +1,18 @@
+  let homeScore = 0;
+    let guestScore = 0;
+
+    function addHome(points) {
+      homeScore += points;
+      document.getElementById('home-score').textContent = homeScore;
+    }
+
+    function addGuest(points) {
+      guestScore += points;
+      document.getElementById('guest-score').textContent = guestScore;
+    }
+    function newGame() {
+      homeScore = 0;
+      guestScore = 0;
+      document.getElementById('home-score').textContent = homeScore;
+      document.getElementById('guest-score').textContent = guestScore;
+    }
